a98ce8f8cfa7be9d76a34db829e3bd90f707b45b
taiz <walter.kerly@protonmail.com>
2023-04-25 20:02:51 -0400

    Add additional checkboxes and boss filtering

--- src/site/index.js
+++ src/site/index.js
@@ -1,12 +1,60 @@
 //console.log(Isaac.actives["The Sad Onion"].moreDesc);
-const quality = document.getElementById("quality");
 const itemsContainer = document.getElementById("items");
+const quality = document.getElementById("quality");
+const clearSatan = document.getElementById("boss_satan");
+const clearSatan = document.getElementById("boss_lamb");
+const clearSatan = document.getElementById("boss_isaac");
+const clearSatan = document.getElementById("boss_bluebaby");
+const clearSatan = document.getElementById("boss_megasatan");
+const clearSatan = document.getElementById("boss_hush");
+const clearSatan = document.getElementById("boss_bossrush");
+const clearSatan = document.getElementById("boss_mother");
+const clearSatan = document.getElementById("boss_delirium");
+const clearSatan = document.getElementById("boss_greed");
+const clearSatan = document.getElementById("boss_greedier");
+const clearSatan = document.getElementById("boss_beast");
+const clearSatan = document.getElementById("boss_heart");
+const currentBoss = {
+	"Satan": false,
+	"Lamb": false,
+	"Isaac": false,
+	"Blue Baby": false,
+	"Mega Satan": false,
+	"Hush": false,
+	"Boss Rush": false,
+	"Mother": false,
+	"Delirium": false,
+	"Greed": false,
+	"Greedier": false,
+	"Beast": false,
+	"Heart": false
+}
+const bossHTMLtoJS = {
+	"boss_satan": "Satan",
+	"boss_lamb": "Lamb",
+	"boss_isaac": "Isaac",
+	"boss_bluebaby": "Blue Baby",
+	"boss_megasatan": "Mega Satan",
+	"boss_hush": "Hush",
+	"boss_bossrush": "Boss R,
+	"boss_mother": "Mother",
+	"boss_delirium": "Delirium",
+	"boss_greed": "Greed",
+	"boss_greedier": "Greedier",
+	"boss_beast": "Beast",
+	"boss_heart": "Heart"
+}
+
 let currentQuality = 0;
 
 quality.addEventListener("change", (event) => {
 	console.log(event.target.value);
 	currentQuality = event.target.value;
 	renderItems();
+});
+
+clearSatan.addEventListener("change", (event) => {
+	currentBoss["Satan"] = event.target.value;
 });
 
 function itemBuilder(name, itemid, pickup, quality, moreDesc, unlock) {
@@ -21,9 +69,7 @@ function itemBuilder(name, itemid, pickup, quality, moreDesc, unlock) {
 function renderItems() {
 	itemsContainer.innerHTML = "";
 	for (let item in Isaac.actives) {
-		//let itemQuality = Isaac.actives[item].quality.split(" ")[1];
-		//if (Isaac.actives[item].quality
-		if (checkQuality(item))
+		if (checkQuality(item) && checkBosses(item))
 			itemsContainer.appendChild(itemBuilder(Isaac.actives[item].name, "","","","",""));
 	}
 }
@@ -31,6 +77,17 @@ function renderItems() {
 function checkQuality(item) {
 	if (Isaac.actives[item].quality == "NA") return false;
 	else return (parseInt(Isaac.actives[item].quality.split(" ")[1]) >= currentQuality)
+}
+
+function checkBosses(item) {
+	if (Isaac.actives[item].unlock.search("Unlock") < 0) return false;
+	for (let boss in currentBoss) {
+		if (currentBoss[boss]) {
+			console.log(boss);
+			if (Isaac.actives[item].unlock.search(boss) > 0) return true;
+		}
+	}
+	return false;
 }
 
 renderItems();
