4519e6e54327db22794e2e7652574c2973934e4e
taiz <walter.kerly@protonmail.com>
2023-05-02 02:06:26 -0400

    Add minify script and files

---
+++ src/site/min/build.sh
@@ -0,0 +1,10 @@
+# JS Uglifier, this is done to consolodate everything into a small package for less requests/faster loading
+# Installation: npm install uglify-js -g
+# Repo: https://github.com/mishoo/UglifyJS
+uglifyjs ../lib/isaac.js ../scripts/buttons.js ../scripts/filters.js ../scripts/popup.js ../scripts/navigation.js ../scripts/main.js -c > min.js
+
+# CSS Uglifier, same as above
+# Installation: npm install uglifycss -g
+# Repo: https://github.com/fmarcia/uglifycss
+uglifycss --ugly-comments ../styles/main.css ../styles/navbar.css ../styles/navigation.css ../styles/popup.css > min.css
+
